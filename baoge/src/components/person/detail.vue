ffa03ffbe2b7f242824e4b3784fa7f99e0b51fe3
aiqiaodaima <754925121@qq.com>
2018-11-23 18:40:14 +0800

    2018/11/23

--- baoge/src/components/person/detail.vue
+++ baoge/src/components/person/detail.vue
@@ -2,17 +2,26 @@
   <div class="contain">
     <mu-container class="demo-container">
       <mu-row gutter>
-        <mu-col span="4">
-          <div class="grid-cell" @click="alert"></div>
+        <mu-col span="6">
+          <div class="grid-cell">
+            <mu-select v-model="normal.value1" full-width>
+              <mu-option v-for="(option,index) in options" :key="option" :label="option" :value="option" no-data-text="normal.value1"></mu-option>
+            </mu-select>
+          </div>
         </mu-col>
-        <mu-col span="3">
-          <div class="grid-cell"></div>
-        </mu-col>
-        <mu-col span="5">
+        <mu-col span="6">
           <div class="grid-cell">
             <mu-date-input action-icon="today" v-model="value10" type="month" container="dialog" full-width></mu-date-input>
           </div>
         </mu-col>
+      </mu-row>
+      <mu-row gutter>
+        <mu-col span="12">
+          <div class="grid-cell">
+            <span class="tips"> {{normal.value2}}</span>
+          </div>
+        </mu-col>
+
       </mu-row>
     </mu-container>
   </div>
@@ -21,6 +30,25 @@
 export default {
   data() {
     return {
+      options: [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+        "Option 7",
+        "Option 8",
+        "Option 9",
+        "Option 10"
+      ],
+      normal: {
+        value1: "筛选",
+        value2: "本月：收入￥1000:00",
+        value3: "",
+        value4: "Option 1",
+        value5: "Option 2"
+      },
       value7: undefined,
       value8: undefined,
       value9: undefined,
@@ -36,6 +64,14 @@ export default {
 <style lang="less" scoped>
 input {
   text-align: center;
+}
+.tips {
+  display: block;
+  width: 100%;
+  height: 100%;
+  line-height: 36px;
+  padding: 0 10px;
+  font-size: 14px;
 }
 .demo-container {
   .row {
@@ -55,6 +91,9 @@ input {
 }
 
 /deep/ .mu-text-field-input {
+  text-align: center;
+}
+/deep/ .mu-select-input {
   text-align: center;
 }
 </style>
